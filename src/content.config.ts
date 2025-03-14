57b59cd3c0c366ff281ae30d295c65da064063e2
Andrew McInnes <acmcinnes@outlook.com>
2025-03-14 13:51:07 +1000

    Updated blog content
    - fixed up Blog collection so ID slug doesn't include filepath
    - added additional frontmatter for blog thumbnails
    - updated Blog index to include post thumbnails

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,17 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 export const collections = {
-	blog: defineCollection({})
+	blog: defineCollection({
+    loader: glob({ pattern: "**/*.mdoc", base: "./src/content/blog" }),
+    schema: ({ image }) => z.object({
+      title: z.string(),
+      thumbnail: z.object({
+        src: image(),
+        alt: z.string(),
+      }),
+      date: z.date(),
+      description: z.string(),
+    }),
+  })
 };
